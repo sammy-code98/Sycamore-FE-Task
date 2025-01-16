83534fc78066a206e8f1d24ab1ab6e80cdca6bc7
sammy-code98 <sammysmart98@yahoo.com>
2025-01-16 20:11:40 +0100

    setup sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav class="p-4 bg-gray-800 text-white">
-      <router-link to="/" class="mr-4">Home</router-link>
-      <router-link to="/settings">Settings</router-link>
-    </nav>
+    <Sidebar/>
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import Sidebar from './components/Sidebar.vue'
 export default defineComponent({
   name: 'App',
+   components: {
+    Sidebar,
+  }
 });
 </script>
